589f44850cd9bfa953a9666dc32c486801990f4c
parsher <26663229+parsher@users.noreply.github.com>
2020-02-19 13:41:18 +0900

    split aop

--- src/com/luv2code/aopdemo/aspect/MyCloudLogAsyncAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyCloudLogAsyncAspect.java
@@ -2,17 +2,15 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-public class MyDemoLoggingAspect {
-	// before order is undefined, has no order
-	// let's start with and @Before advice
-	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
-	public void beforeAddAccountAdvice() {
-		System.out.println("\n====> Executing @Before advice on addAccount()");
-	}
+public class MyCloudLogAsyncAspect {
 
+	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
+	public void logToCloudAsync() {
+		System.out.println("\n====> Logging to Cloud in async fashion");
+	}
+	
 }
